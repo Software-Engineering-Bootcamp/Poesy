ccf3e1cab079f87e5c130f7c5332941c714e6f0d
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-07-25 00:51:17 +0300

    Added Toggle State

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import PoemsContainer from "./PoemsContainer";
 import NewPoemForm from "./NewPoemForm";
 
 function App() {
+
+  const [autoToggle, setAutoToggle] = useState(false);
+
+
+  const toggleForm = (e)=>{
+    setAutoToggle(!autoToggle);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
-        <button>Show/hide new poem form</button>
-        {true ? <NewPoemForm /> : null}
+        <button onClick={toggleForm}>
+          Show/hide new poem form
+        </button>
+        {autoToggle ? <NewPoemForm /> : null}
       </div>
       <PoemsContainer />
     </div>
