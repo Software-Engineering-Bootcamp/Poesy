4a3278f3d99a3d5680e90964ba856f4d2207223f
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-07-25 23:04:03 +0300

    Display fetched poems

--- src/components/PoemsContainer.js
+++ src/components/PoemsContainer.js
@@ -1,10 +1,19 @@
 import React from "react";
 import Poem from "./Poem";
 
-function PoemsContainer() {
+function PoemsContainer({poems, removePoem, addToFavorites}) {
   return (
     <div className="poems-container">
-      {/* render a list of <Poem> components in here */}
+      {poems.map(poem => {
+        return (
+          <Poem 
+            key={poem.id} 
+            poem={poem} 
+            removePoem={removePoem} 
+            addToFavorites={addToFavorites} 
+          />
+        )
+        })}
     </div>
   );
 }
